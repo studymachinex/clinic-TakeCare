7d091ae915fb00245f19190830de7341eaef0990
Carlos Gabriel <carlosufpa10@gmail.com>
2022-12-28 18:29:05 -0300

    add

---
+++ src/main/java/com/ibm/takecare/TakecareApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.takecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakecareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakecareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/takecare/TakecareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.takecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakecareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
