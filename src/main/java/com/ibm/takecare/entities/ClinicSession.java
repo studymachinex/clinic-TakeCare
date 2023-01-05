056f1671003cf1d6191392f9c3b4a603562a37df
Carlos Gabriel <carlosufpa10@gmail.com>
2023-01-05 13:03:03 -0300

    :fireworks

--- src/main/java/com/ibm/takecare/entities/ClinicSession.java
+++ src/main/java/com/ibm/takecare/entities/ClinicSession.java
@@ -1,5 +1,13 @@
 package com.ibm.takecare.entities;
 
+import java.util.Date;
+import jakarta.persistence.Entity;
+
+@Entity
 public class ClinicSession {
-    
+
+    //Inicialmente vai dar para agendar o dia, horario vai ser feature kkkkkk
+    Date date;
+
+
 }
