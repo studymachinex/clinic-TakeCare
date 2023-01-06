4dc2cdbbfba3a60dbfbf0b81d0d9ee6244ebb5bf
Carlos Gabriel <carlosufpa10@gmail.com>
2023-01-06 13:22:09 -0300

    criando Status da sessao

--- src/main/java/com/ibm/takecare/resources/enums/ClinicSessionStatus.java
+++ src/main/java/com/ibm/takecare/resources/enums/ClinicSessionStatus.java
@@ -1,6 +1,16 @@
 package com.ibm.takecare.resources.enums;
 
 public enum ClinicSessionStatus {
-    SESSION_STARTED,
-    SESSION_OFF
+    SESSION_STARTED("Session online"),
+    SESSION_NOT_STARTED("Session offline");
+
+    private String status;
+
+    private ClinicSessionStatus(String status){
+        this.status = status;
+    }
+
+    public String getStatus(){
+        return status;
+    }
 }
