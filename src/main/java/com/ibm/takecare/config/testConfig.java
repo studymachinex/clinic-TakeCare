274e94192e39d1d1b49359e1e79abd85870d5600
Carlos Gabriel <carlosufpa10@gmail.com>
2023-01-06 14:51:31 -0300

    inputando dados para testar os endpoints

--- src/main/java/com/ibm/takecare/config/testConfig.java
+++ src/main/java/com/ibm/takecare/config/testConfig.java
@@ -1,10 +1,13 @@
 package com.ibm.takecare.config;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
+import com.ibm.takecare.entities.ClinicSession;
 import com.ibm.takecare.repositories.ClinicSessionRepository;
 
 @Configuration
@@ -14,13 +17,19 @@ public class testConfig implements CommandLineRunner{
     @Autowired
     private ClinicSessionRepository clinicSessionRepository;
     
-    // LocalDate contractDate = LocalDate.parse(data1, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    // LocalDate/time.parse(variavel a ser parseada, DateTimeFormatter.ofPattern("dd/MM/yyyy") ou HH:mm:ss
 
 
     // Implementar método run com os dados para teste.
-
     @Override
     public void run(String... args){
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+
+        ClinicSession clinic = new ClinicSession
+        ( LocalDate.parse("06/01/2023", dateFormatter)
+        , LocalTime.parse("03:00", timeFormatter)
+        , null);
 
     }
 }
